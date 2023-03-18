e8fe8f5af426d1ecd7ca1574e8db4dc3418a6cfc
Razan Badauod <rooz098e@gmail.com>
2023-03-18 16:49:42 +0300

    Razan Commit

--- src/main/java/main/B10_Group3.java
+++ src/main/java/main/B10_Group3.java
@@ -5,6 +5,6 @@ public class B10_Group3 {
 
     public static void main(String[] args) {
         System.out.println("Hello, I'm Maha :) ");
-        system.out.println("Hello, I'm Razan <3 ");
+        System.out.println("Hello, I'm Razan <3 ");
     }
 }
